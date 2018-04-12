237be9670909a6dab5918a1954aa7f92b3b347bb
Alexander Kachkaev <alexander@kachkaev.ru>
2018-04-12 18:21:15 +0100

    Fix discovery of elm-format by better configuring execa

--- src/parser.js
+++ src/parser.js
@@ -5,6 +5,8 @@ const execa = require("execa");
 function formatTextWithElmFormat(text) {
   return execa.sync("elm-format", ["--stdin"], {
     input: text,
+    preferLocal: true,
+    localDir: __dirname,
     stripEof: false
   });
 }
