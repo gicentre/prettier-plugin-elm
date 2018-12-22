4b794c995a1b496cba3df24c0ef5c2b2a26e27e2
Alexander Kachkaev <alexander@kachkaev.ru>
2018-12-22 20:34:03 +0000

    Set up Azure Pipelines (#9)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
+"use strict";
+
+const REPORT_TEST_RESULTS_AND_COVERAGE = !!process.env
+  .REPORT_TEST_RESULTS_AND_COVERAGE;
+
 module.exports = {
+  collectCoverage: REPORT_TEST_RESULTS_AND_COVERAGE,
   collectCoverageFrom: ["dist/**/*.js", "!dist/**/*.test.js"],
+  coverageReporters: ["cobertura", "html", "text"],
+  reporters: REPORT_TEST_RESULTS_AND_COVERAGE
+    ? ["default", "jest-junit"]
+    : ["default"],
 };
