cb44671616b0882ecfc70f4f2db7de9def867ee1
Alexander Kachkaev <alexander@kachkaev.ru>
2018-12-22 19:30:22 +0000

    Enable cobertura coverage and fix condition in pipelines

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ const REPORT_TEST_RESULTS_AND_COVERAGE = !!process.env
 module.exports = {
   collectCoverage: REPORT_TEST_RESULTS_AND_COVERAGE,
   collectCoverageFrom: ["dist/**/*.js", "!dist/**/*.test.js"],
+  coverageReporters: ["cobertura", "html", "text"],
   reporters: REPORT_TEST_RESULTS_AND_COVERAGE
     ? ["default", "jest-junit"]
     : ["default"],
