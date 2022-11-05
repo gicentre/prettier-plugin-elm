4be80214d761f284f3fc27b0113dc062c82469c1
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-05 15:43:20 +0000

    Tweak comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
   reportUnusedDisableDirectives: true,
   rules: {
     /* eslint-disable @typescript-eslint/naming-convention */
-    "unicorn/prefer-module": "off", // Can be enabled after switching to ESM or Node.js >=16
-    "unicorn/prefer-node-protocol": "off", // Can be enabled after switching to ESM or Node.js >=16
+    "unicorn/prefer-module": "off", // Can be enabled after switching to ESM
+    "unicorn/prefer-node-protocol": "off", // Can be enabled after switching to ESM or dropping Node.js 14
     /* eslint-enable @typescript-eslint/naming-convention */
   },
 };
