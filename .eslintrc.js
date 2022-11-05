dc7ac663d11f81fe828930c44b15424df1ba67a2
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-05 15:43:37 +0000

    Improve dev setup (#34)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
+/** @type{import("eslint").Linter.Config} */
 module.exports = {
   extends: ["@kachkaev/eslint-config-base"],
+  reportUnusedDisableDirectives: true,
   rules: {
     /* eslint-disable @typescript-eslint/naming-convention */
-    "unicorn/prefer-json-parse-buffer": "off",
-    "unicorn/prefer-module": "off",
-    "unicorn/prefer-node-protocol": "off",
+    "unicorn/prefer-module": "off", // Can be enabled after switching to ESM
+    "unicorn/prefer-node-protocol": "off", // Can be enabled after switching to ESM or dropping Node.js 14
     /* eslint-enable @typescript-eslint/naming-convention */
   },
 };
