1ad8d8afb278e4866a219ddedd3414aa78f38e10
Alexander Kachkaev <alexander@kachkaev.ru>
2022-11-05 19:24:40 +0000

    Tweak CI

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 /** @type {import("jest").Config} */
 export default {
-  collectCoverage: process.env.TEST_COVERAGE === "true",
+  collectCoverage: process.env.COVERAGE === "true",
   collectCoverageFrom: ["dist/**/*.js", "!dist/**/*.test.js"],
   testRegex: ".test.js$",
 };
