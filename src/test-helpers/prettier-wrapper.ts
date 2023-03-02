15ba53edff32e0243d8f18f64ec163a1cc3e862b
Alexander Kachkaev <alexander@kachkaev.ru>
2023-03-02 12:33:19 +0000

    Update devDependencies

--- src/test-helpers/prettier-wrapper.ts
+++ src/test-helpers/prettier-wrapper.ts
@@ -1,14 +1,17 @@
-import type { Options } from "prettier";
+import type * as prettier from "prettier";
 
 export const getPrettier = async () => {
   switch (process.env.PRETTIER_MAJOR_VERSION) {
     case "2": {
-      return await import("prettier-v2");
+      // there is a slight type mismatch in format Options between v2 and v3
+      return (await import(
+        "prettier-v2"
+      )) as unknown as typeof import("prettier");
     }
     case "1": {
       return (
-        // there is a slight type mismatch in format Options between v1 and v2
-        (await import("prettier-v1")) as unknown as typeof import("prettier-v2")
+        // there is a slight type mismatch in format Options between v1 and v3
+        (await import("prettier-v1")) as unknown as typeof import("prettier")
       );
     }
     default: {
@@ -19,7 +22,7 @@ export const getPrettier = async () => {
 
 export const format = async (
   source: string,
-  options?: Options,
+  options?: prettier.Options,
 ): Promise<string> => {
   const prettier = await getPrettier();
 
@@ -27,6 +30,5 @@ export const format = async (
     return prettier.format(source, options);
   }
 
-  // eslint-disable-next-line @typescript-eslint/await-thenable -- v3 typings are currently copied from v2
   return await prettier.format(source, options);
 };
